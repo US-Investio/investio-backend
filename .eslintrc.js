729c9da73c73c66bd0e05ddb2a26cdcfe8228e6f
t-junnes <tjunne@naver.com>
2023-12-29 14:51:36 +0900

    chore: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,28 +3,30 @@ const WARNING = 1;
 const ERROR = 2;
 
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   rules: {
+    'no-shadow': OFF,
+    '@typescript-eslint/no-shadow': ERROR,
     '@typescript-eslint/interface-name-prefix': OFF,
     '@typescript-eslint/explicit-function-return-type': OFF,
     '@typescript-eslint/explicit-module-boundary-types': OFF,
-    '@typescript-eslint/no-explicit-any': OFF,
     'prettier/prettier': WARNING,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
   },
 };

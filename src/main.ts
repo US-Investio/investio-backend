3c6a567d27424c04d95388af80ec32acb80fb12d
t-junnes <tjunne@naver.com>
2024-02-04 02:28:57 +0900

    feat: configure swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  app.setGlobalPrefix('/v1');
+
+  const docsConfig = new DocumentBuilder()
+    .setTitle('Investio API')
+    .setDescription('API documents for Investio project')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docsConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(8080);
 }
 bootstrap().catch((err) => console.error(err));
